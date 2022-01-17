b13a0560fea9de829143b95fb13b7603ca740466
Daniel Mbaluka <danielmbaluka@gmail.com>
2022-01-17 09:09:46 +0300

    add a build script for checking if version was manually incremented

---
+++ scripts/CheckVersion.ps1
@@ -0,0 +1,17 @@
+$project = ".\PizzaApi.csproj"
+
+[xml]$xmlDoc = Get-Content $project
+
+$projectVersionString = $xmlDoc.Project.PropertyGroup.AppVersion
+$projectVersionNumber = $projectVersionString -replace "\."
+
+# Get the current version of the latest public NuGet package
+$url = "https://api.nuget.org/v3/registration3/microsoft.openapi.odata/index.json"
+$nugetIndex = Invoke-RestMethod -Uri $url -Method Get
+$currentPublishedVersionString = $nugetIndex.items[0].upper
+$currentPublishedVersionNumber = $currentPublishedVersionString -replace "\."
+
+if ($projectVersionNumber -le  $currentPublishedVersionNumber) {
+	Write-Host "The version number has not been incremented. Aborting build."
+	Exit 1
+}
