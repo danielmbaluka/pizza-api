396ed1a1c47ad52e70204ba365b56bc6505db7d2
Daniel Mbaluka <danielmbaluka@gmail.com>
2022-01-19 16:13:55 +0300

    update nuget index to consult for latest package version

--- scripts/CheckVersion.ps1
+++ scripts/CheckVersion.ps1
@@ -6,12 +6,15 @@ $projectVersionString = $xmlDoc.Project.PropertyGroup.Version
 $projectVersionNumber = $projectVersionString -replace "\."
 
 # Get the current version of the latest public NuGet package
-$url = "https://api.nuget.org/v3/registration3/microsoft.openapi.odata/index.json"
+$url = "https://api.nuget.org/v3/registration5-semver1/pizzaapi/index.json"
 $nugetIndex = Invoke-RestMethod -Uri $url -Method Get
 $currentPublishedVersionString = $nugetIndex.items[0].upper
 $currentPublishedVersionNumber = $currentPublishedVersionString -replace "\."
 
 if ($projectVersionNumber -le  $currentPublishedVersionNumber) {
-	Write-Host "The version number has not been incremented. Aborting build."
+	Write-Error "The version number has not been incremented. Aborting build."
 	Exit 1
+}
+else {
+	Write-Host "Validated that the version has been updated from $currentPublishedVersionNumber to $projectVersionNumber" -ForegroundColor Green
 }
