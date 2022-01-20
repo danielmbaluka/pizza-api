68b6d0384f1e204087468941a601f7beddfed89b
Daniel Mbaluka <danielmbaluka@gmail.com>
2022-01-20 13:24:22 +0300

    parameterize check version script with project version

--- scripts/CheckVersion.ps1
+++ scripts/CheckVersion.ps1
@@ -1,9 +1,20 @@
-$project = ".\PizzaApi.csproj"
 
-[xml]$xmlDoc = Get-Content $project
+<#
+.Synopsis
+    Check if version has been bumped
+.Description
+    Check if the project version has been bumped by comparing current project version 
+	with what is publised on nuget.org
+.Parameter projectVersion
+    Current project version
+#>
 
-$projectVersionString = $xmlDoc.Project.PropertyGroup.Version
-$projectVersionNumber = $projectVersionString -replace "\."
+Param(
+	[parameter(Mandatory = $true)]
+	[string]$projectVersion
+)
+
+$projectVersionNumber = $projectVersion -replace "\."
 
 # Get the current version of the latest public NuGet package
 $url = "https://api.nuget.org/v3/registration5-semver1/pizzaapi/index.json"
